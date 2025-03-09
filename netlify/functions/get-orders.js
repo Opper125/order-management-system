dd26622b1b9e9b2d72f2e8daa4020cd50e1d3ede
Opper125 <crakb68@gmail.com>
2025-03-10 02:48:46 +0630

    Delete netlify/functions directory

--- netlify/functions/get-orders.js
+++
@@ -1,8 +0,0 @@
-const EventEmitter = require('events');
-const orderEmitter = new EventEmitter();
-let orders = [];
-
-exports.handler = async (event, context) => {
-    if (event.httpMethod !== 'GET') return { statusCode: 405, body: 'Method not allowed' };
-    return { statusCode: 200, body: JSON.stringify(orders) };
-};
